21b9696167e4e779b11bbd2ec158f322f13a652f
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2020-07-23 21:34:45 +0530

    Add files via upload

---
+++ Day_7/todo/todo1.js
@@ -0,0 +1,19 @@
+console.log("hello")
+
+//  create a two variables
+const button  = document.querySelector("#btn");
+const list = document.querySelector('#list');
+
+button.onclick = function(){
+    let item = document.querySelector("#todo").value;
+    console.log(item , typeof(item));
+    let text = document.createTextNode(item)
+    let listItem = document.createElement
+    ('li');
+    listItem.appendChild(text);
+    list.appendChild(listItem);
+    document.forms.myForm.reset();
+
+}
+
+// button.addEventListener('click')
