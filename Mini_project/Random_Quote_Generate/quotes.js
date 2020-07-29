3dc2fe0373cc30b40ca69da22c8374282bc1aca2
BHUBESH SR <55022929+bhubesh757@users.noreply.github.com>
2020-07-29 12:35:33 +0530

    Add files via upload

---
+++ Mini_project/Random_Quote_Generate/quotes.js
@@ -0,0 +1,42 @@
+var quotes = [
+  {
+    quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    source: "Martin Fowler",
+    citation: "Address to United Nations",
+    year: 2013,
+    tags: "co"
+  },
+  {
+    quote: '<font size="6px">First, solve the problem. Then, write the code.</font>',
+    source: "Hillary Clinton",
+    citation: "2016 Presidential Concession Speech",
+    year: 2016,
+    tags: "politics"
+  },
+  {
+    quote: '<font size="12px">Experience is the name everyone gives to their mistakes</font>',
+    source: "Chimamanda Ngozi Adichie",
+    citation: "We Should All Be Feminsts",
+    year: 2014,
+    tags: "feminism"
+  },
+  {
+    quote: "Java is to JavaScript what car is to Carpet.",
+    source: "Maya Angelou",
+    citation: "Phenomenal Woman",
+    year: 1995,
+    tags: "poetry"
+  },
+  {
+    quote: "Code is like humor. When you have to explain it, it’s bad.",
+    source: "Melinda Gates",
+    citation: "Powerful Voices Luncheon",
+    year: 2003,
+    tags: "feminism"
+  },
+   {
+    quote: "Simplicity is the soul of efficiency",
+    source: "Unknown",
+    tags: "politics"
+  }
+];
